90cf2e1e9d2d1e78d2dbf9f43cbf517e54d14bf5
Yassin Makrai <makrai.yassin@gmail.com>
2022-10-09 10:41:47 +0200

    feature filter in catalogue page

--- src/app/components/product-component/list-product-catalogue/list-product-catalogue.component.ts
+++ src/app/components/product-component/list-product-catalogue/list-product-catalogue.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {IProduct} from "../../../models/IProduct";
 import {ProductService} from "../../../services/product.service";
 import {ActivatedRoute} from "@angular/router";
+import {Categories} from "../../../models/Categories";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-list-product-catalogue',
@@ -11,6 +13,14 @@ import {ActivatedRoute} from "@angular/router";
 export class ListProductCatalogueComponent implements OnInit {
 
   private _productList!: IProduct[];
+  categories = Categories;
+
+  filters = new FormGroup({
+    category:new FormControl("all"),
+    available: new FormControl(true),
+    unavailable: new FormControl(true),
+    sortBy:new FormControl("category"),
+  })
 
   constructor(
     private _productService : ProductService,
@@ -49,4 +59,53 @@ export class ListProductCatalogueComponent implements OnInit {
   borrowsProgress(product : IProduct): number {
     return product.borrowList.filter(b=>!b.returned).length
   }
+
+  tradCategory(category : string) {
+    switch (category){
+      case "BOOK":
+        return "Livre"
+      case "BOARDGAME":
+        return "Jeux de société"
+      case "VIDEOGAME":
+        return "Jeux vidéo"
+      case "MOVIE":
+        return "Film"
+      case "MUSIC":
+        return "Musique"
+      default:
+        return "Livre"
+    }
+  }
+
+
+  filter(){
+    if(!!this.filters.value.available && !!this.filters.value.unavailable){
+      this.products = []
+      return
+    }else{
+      if(this.filters.value.sortBy == "nameAsc") {
+        this._productList.sort((a, b) => (a.name > b.name) ? 1 : (a.name == b.name) ? 0 :-1).slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+      }else if(this.filters.value.sortBy == "nameDesc") {
+        this._productList.sort((a, b) => (a.name < b.name) ? 1 :  (a.name == b.name) ? 0 : -1).slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+      }else if(this.filters.value.sortBy == "category") {
+        this._productList.sort((a, b) => (a.category > b.category) ? 1 : (a.category == b.category) ? 0 : -1 ).slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+      }
+
+      if(this.filters.value.category != "all"){
+        this.products = this._productList.filter(p=>p.category==this.filters.value.category).slice((this.page-1) * this.pageSize, (this.page -1) * this.pageSize + this.pageSize);
+      }else{
+        this.products = this._productList.slice((this.page-1) * this.pageSize, (this.page -1) * this.pageSize + this.pageSize);
+      }
+
+      // if(this.filters.value.available == true){
+      //   this.products =  this.products.filter(a=> {a.quantity>0 && this.borrowsProgress(a)<a.quantity}).slice((this.page-1) * this.pageSize, (this.page -1) * this.pageSize + this.pageSize);
+      // }
+      // else if(!!this.filters.value.unavailable){
+      //   this.products =  this.products.filter(a=> {a.quantity == 0 || this.borrowsProgress(a) >= a.quantity}).slice((this.page-1) * this.pageSize, (this.page -1) * this.pageSize + this.pageSize);
+      // }
+
+    }
+
+  }
+
 }
